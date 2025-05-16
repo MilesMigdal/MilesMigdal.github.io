5ffc6d2ad17e1311f9abeea1cb57ff05fe626eed
MilesMigdal <157547109+MilesMigdal@users.noreply.github.com>
2025-05-16 00:49:56 +0000

    saving walker

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -1,8 +1,8 @@
 /* global $, sessionStorage */
 
 $(document).ready(runProgram); // wait for the HTML / CSS elements of the page to fully load, then execute runProgram()
-  
-function runProgram(){
+
+function runProgram() {
   ////////////////////////////////////////////////////////////////////////////////
   //////////////////////////// SETUP /////////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
@@ -10,15 +10,20 @@ function runProgram(){
   // Constant Variables
   var FRAME_RATE = 60;
   var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
-  
+  var KEY = {
+    LEFT: 37,
+    RIGHT: 39,
+    UP: 38,
+    DOWN: 40,
+  };
+
+var walker = 
+
   // Game Item Objects
 
-
   // one-time setup
-  var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-  $(document).on('keydown', handleKeyDown);                           // change 'eventType' to the type of event you want to handle
-
-
+  var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL); // execute newFrame every 0.0166 seconds (60 Frames per second)
+  $(document).on("keydown", handleKeyDown); // change 'eventType' to the type of event you want to handle
 
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
@@ -28,23 +33,27 @@ function runProgram(){
   On each "tick" of the timer, a new frame is dynamically drawn using JavaScript
   by calling this function and executing the code inside.
   */
-  function newFrame() {
-    
+  function newFrame() {}
 
-  }
-  
   /* 
   Called in response to events.
   */
   function handleKeyDown(event) {
-console.log(handleKeyDown)
+    if (event.which === KEY.DOWN) {
+      console.log("Down Pressed");
+    } else if (event.which === KEY.UP) {
+      console.log("Up Pressed");
+    } else if (event.which === KEY.LEFT) {
+      console.log("Left Pressed");
+    } else if (event.which === KEY.RIGHT) {
+      console.log("Right Pressed");
+    }
   }
 
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
 
-  
   function endGame() {
     // stop the interval timer
     clearInterval(interval);
@@ -52,5 +61,4 @@ console.log(handleKeyDown)
     // turn off event handlers
     $(document).off();
   }
-  
 }
